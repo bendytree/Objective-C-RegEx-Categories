ccb800159e04cfda97fbcf7d524511384d04ea76
Josh Wright <help@bendytree.com>
2013-11-30 17:26:31 -0600

    added replacement case sensitive test

--- RegexDummyTests/Replace.m
+++ RegexDummyTests/Replace.m
@@ -20,6 +20,18 @@
     XCTAssertEqualObjects(result, @"meow meow!", @"Expected to replace both barks.");
 }
 
+- (void) test_regex_replace_replaces_with_back_references
+{
+    NSString* result = [@"5551234567" replace:RX(@"(\\d{3}).?(\\d{3}).?(\\d{4})") with:@"1 ($1) $2-$3"];
+    XCTAssertEqualObjects(result, @"1 (555) 123-4567", @"Expected a properly formatted phone number.");
+}
+
+- (void) test_regex_replace_is_case_sensitive
+{
+    NSString* result = [@"Dogs are dogs." replace:RX(@"dogs") with:@"cats"];
+    XCTAssertEqualObjects(result, @"Dogs are cats.", @"Expected replace to ignore casing.");
+}
+
 - (void) test_replace_on_regex_replaces_all_occurances
 {
     NSString* result = [RX(@"ruf+") replace:@"ruff ruf!" with:@"meow"];
